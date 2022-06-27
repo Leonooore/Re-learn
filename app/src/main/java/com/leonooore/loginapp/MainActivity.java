11c5f5932167542e4c556e1c0c4128bbdab5a3ab
Eleonora Fetisova <elnora.fet@gmail.com>
2022-06-28 02:25:55 +0300

    [add] project "Login App" V3

--- app/src/main/java/com/leonooore/loginapp/MainActivity.java
+++ app/src/main/java/com/leonooore/loginapp/MainActivity.java
@@ -3,12 +3,35 @@ package com.leonooore.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String USER_ADMIN = "admin";
+    public static final String PASSWORD_ADMIN = "123123";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView user = findViewById(R.id.viewEditTextLogin);
+        TextView password = findViewById(R.id.viewEditTextPassword);
+
+        MaterialButton login = findViewById(R.id.buttonLogin);
+
+        login.setOnClickListener(view -> {
+            if(user.getText().toString().equals(USER_ADMIN) && password.getText().toString().equals(PASSWORD_ADMIN)) {
+                //correct
+                Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+            } else
+                //incorrect
+                Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
